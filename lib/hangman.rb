c317d9d5308c15a3cadb65b94836de0ca076a9de
AlexP <alexpitzer42@gmail.com>
2015-09-27 15:47:17 -0700

    'third commit'

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,6 @@
-require "hangman/version"
+require_relative "hangman/version"
 
 module Hangman
   # Your code goes here...
 end
-
 require_relative "./hangman/game.rb"
--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -7,52 +7,56 @@ module Hangman
 				dict << line if line.length.between?(5,12)
 			end
 			@dictionary = dict
-			@word = @dictionary.sample.downcase
+			@word = @dictionary.sample.downcase[0..-3] #strips away \r and \n characters
 			@guesses = 5
 			@guess_map = Array.new(@guesses) {false}
 
-			setup
+			puts welcome_message
 			game_loop unless game_over?
 
 		end
 
-		private
 
-		def setup
-			puts "Welcome to hangman!"
+		def welcome_message
+			"Welcome to hangman!"
 		end
 
 		def game_loop
 			puts "You have #{@guesses} guesses left."
 			puts "Your word currently looks like: #{represent_word}"
 			print "please enter a letter: "
-			current_word = handle_guess(gets.chomp)
+			puts ""
+			letter = sanitize_guess(gets.chomp)
+			handle_guess(letter)
+
 		end
 
-		def game_over
-			return true if (@guesses < 0 || )
+		def game_over?
+			return true if (@guesses <= 0 || represent_word == @word)
+			false
 		end
 
 		def represent_word
-			@
+			#if letter is guessed correctly, give the letter in the representation.
+			@word.split('').each_with_index.map { |word, index|  @guess_map[index] ? word : "_" }.join('')
+		end
+
+		def sanitize_guess(letter)
+			if ( !('a'..'z').include?(letter.downcase) )
+				puts "Try again, with a letter please."
+			    sanitize_guess(letter)
+			else
+				letter
 		end
 
 		def handle_guess(letter)
-			return "Try again, with a valid character. " if (letter.length != 1)
-			letter = letter.downcase
-			cur_word = ""
-			@guesses.each_with_index do |item, index|
-				@guess_map[index] = true if letter == @guesses[index]
-				if @guess_map[index]
-					cur_word << letter
-				else
-					cur_word << "_"
-				end
-			end
-
-			cur_word		
-
-
+  		  if @word.include?(letter)
+  		  	@word.each_with_index do |char, index|
+  		  		@guess_map[index] = true if char == letter 
+  		  	end
+  		  else
+  		  	@guess -= 1
+  		  end
 		end
 	end
 end
